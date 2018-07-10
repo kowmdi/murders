0369c236cef4cb05d963a2c562f3e329a311e162
Khaleel OBrien <khaleelo@blindedbylight.local>
2018-07-10 00:26:50 -0400

    For god sake!

---
+++ wrangle-data.R
@@ -0,0 +1,4 @@
+library(dplyr);library(readr)
+murders <- read_csv("data/murders.csv")
+murders <- murders %>% mutate(region=factor(region),rate=total/population*10^5)
+save(murders,file="rda/murders.rda")
